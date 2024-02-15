a4063978f5ef7d04838bb95d0ce151c16bb1015f
ashram7 <ashram7@hanmail.net>
2024-02-15 14:30:24 +0900

    guidetext

--- ex06_guidetext/js/script1.js
+++ ex06_guidetext/js/script1.js
@@ -1,3 +1,13 @@
 ﻿$(function(){
-	
+	$('.guideText').each(function(){
+		var giudeText = this.defaultValue;
+		// giudeText="이름을 입력해 주세요"
+		var element = $(this);
+		element.focus(function(){
+			if(element.val()===giudeText){
+				element.val('');
+				element.removeClass('guide');
+			}
+		});
+	});
 });
